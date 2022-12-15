424c8c0221f194d3c9819d33bf36854415fd5099
lukasgbe <88949122+lukasgbe@users.noreply.github.com>
2022-12-15 19:47:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var closeButton = document.getElementById('close-button');
+const closeButton = document.getElementById('close-button');
 
 // Listen for a click event on the close button
 closeButton.addEventListener('click', () => {
-    var newsBar = document.getElementById('news-bar');
+    const newsBar = document.getElementById('news-bar');
 
     // Set the display of the news bar to "none" to hide it
     newsBar.style.display = 'none';
